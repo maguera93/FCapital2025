4be1098b85c41243516376794aa966dc1b069a97
randaloth <marc.24144@gmail.com>
2025-05-08 18:39:37 +0200

    Merge branch 'main' of https://github.com/maguera93/FCapital2025

---
+++ Assets/Scripts/Singletones/GlobalVariables.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GlobalVariables", menuName = "Scriptable Objects/GlobalVariables")]
+public class GlobalVariables : ScriptableSingleton<GlobalVariables>
+{
+    [SerializeField]
+    private int m_salary = 1100;
+    [SerializeField]
+    private int m_lifes = 6;
+    [SerializeField]
+    private int m_points = 0;
+
+    public delegate void EnemyDeffeated();
+    public EnemyDeffeated OnEnemyDeffeated;
+
+    public int Salary { get => m_salary; set => m_salary = value; }
+    public int Lifes { get => m_lifes; set => m_lifes = value; }
+    public int Points { get => m_points; set => m_points = value; }
+
+    public void EnemyDeffeatedTriggered()
+    {
+        if (OnEnemyDeffeated != null)
+            OnEnemyDeffeated.Invoke();
+    }
+}
